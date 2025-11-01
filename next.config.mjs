d4bdb0fdd288efe81f31893071507d339332d12d
Zerolight90 <ant7773@gmail.com>
2025-11-01 23:46:20 +0900

    2025_10_30_S3이미지 작업 및 읽음/안읽음 버그 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const BACKEND = process.env.NEXT_PUBLIC_API_BASE;
+const BACKEND = process.env.NEXT_PUBLIC_API_BASE
 
 const nextConfig = {
-    output: 'standalone',
-    eslint: { ignoreDuringBuilds: true },  // ✅ 이 줄 추가
+    // 필요 시 끄기/켜기 옵션들
+    // reactStrictMode: true,
 
     async rewrites() {
         return [
+            // ✅ 백엔드의 모든 /api 경로를 프록시
             { source: '/api/:path*', destination: `${BACKEND}/api/:path*` },
+
+            // (선택) 업로드 이미지를 백엔드에서 서빙할 때
             { source: '/uploads/:path*', destination: `${BACKEND}/uploads/:path*` },
         ];
     },
