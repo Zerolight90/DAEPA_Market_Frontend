dd5c20fda189d9ed683be2589fe8c82d4e452226
yh <ant7773@naver.com>
2025-11-07 10:39:38 +0900

    미들웨이

--- src/app/admin/login/page.js
+++ src/app/admin/login/page.js
@@ -38,7 +38,7 @@ export default function AdminLoginPage() {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:8080/api/admin/login", {
+      const res = await fetch("http://52.79.241.142/api/admin/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
